fe8a2bf3193baee3b8dabedb3eda21b7b2db2352
Matheus de Sousa <math.de.sousa.silva@gmail.com>
2024-07-15 11:41:32 -0300

    first commit

---
+++ src/routes/get-trip-details.ts
@@ -0,0 +1,40 @@
+import { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+import { prisma } from '../lib/prisma'
+import { ClientError } from '../errors/client-error'
+
+export async function getpTripDetails(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().get(
+    '/trips/:tripId',
+    {
+      schema: {
+        params: z.object({
+          tripId: z.string().uuid()
+        })
+      }
+    },
+    async (request, reply) => {
+      const { tripId } = request.params
+
+      const trip = await prisma.trip.findUnique({
+        where: {
+          id: tripId
+        },
+        select: {
+          id: true,
+          destination: true,
+          is_confirmed: true,
+          starts_at: true,
+          ends_at: true
+        }
+      })
+
+      if (!trip) {
+        throw new ClientError('Trip not found.')
+      }
+
+      return { trip }
+    }
+  )
+}
